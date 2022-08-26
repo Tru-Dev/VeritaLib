e2f37a413584f7d739e423225da0035a9cf015fd
TruDev <29785998+Tru-Dev@users.noreply.github.com>
2022-08-26 15:59:21 -0700

    Added substitution hints for non-config variables

--- vscode_plugin.lua
+++ vscode_plugin.lua
@@ -6,6 +6,7 @@ function OnSetText(uri, text)
     end
     local diffs = {}
     local s = 1
+    local dparen_replace = {}
     while true do
         local ss, e, lua = text:find("^#+([^\n]*\n?)", s)
         if not e then
@@ -14,7 +15,18 @@ function OnSetText(uri, text)
             local text2 = text:sub(s, ss)
             for ibegin, iend in text2:gmatch("()$%b()()") do
                 local executed = text2:sub(ibegin + 2, iend - 2)
-                if executed:sub(1, 6) == "config" then
+                for k, v in pairs(dparen_replace) do
+                    print(k, v, executed)
+                    if executed == k then
+                        table.insert(diffs, {
+                            start = s + ibegin - 1,
+                            finish = s + iend - 2,
+                            text = v
+                        })
+                        goto continue
+                    end
+                end
+                if executed:sub(1, 7) == "config." then
                     local to_load = "local config = ...; return " .. executed
                     local loaded = load(to_load)(config)
                     table.insert(diffs, {
@@ -25,12 +37,31 @@ function OnSetText(uri, text)
                 else
                     table.insert(diffs, {
                         start = s + ibegin - 1,
-                        finish = s + iend - 1,
+                        finish = s + iend - 2,
                         text = ""
                     })
                 end
+                ::continue::
             end
             if not e then break end
+        end
+        if text:sub(ss, ss + 3) == "#--=" then
+            ---@cast text string
+            local _, _, line = text:sub(ss, e):find("#--%s*([^\n]*)")
+            for k, v in line:gmatch("([_%a][_%w]*)%s*=%s*(%d+)") do
+                dparen_replace[k] = v
+            end
+            for k, v in line:gmatch("([_%a][_%w]*)%s*=%s*([_%a][_%w]*)") do
+                dparen_replace[k] = v
+            end
+            for k, v in line:gmatch("([_%a][_%w]*)%s*=%s*(\"[_%a][_%w]*\")") do
+                dparen_replace[k] = v
+            end
+            for k, v in line:gmatch("([_%a][_%w]*)%s*=%s*('[_%a][_%w]*')") do
+                dparen_replace[k] = v
+            end
+        else
+            dparen_replace = {}
         end
         table.insert(diffs, {
             start = ss,
